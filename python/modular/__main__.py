c2a59e839d5e210b0ea4b0d22c841723a1e05e39
Yury Shvedov <yshvedov@wimarksystems.com>
2020-03-01 12:30:36 +0300

    Evolute generator arguments
    Use ArgumentParser class to configure paths

--- python/modular/__main__.py
+++ python/modular/__main__.py
@@ -1,8 +1,8 @@
 from .module import Module
 from .version import VERSION
 from yaml import CLoader as Loader, load
-from os import path
-import sys
+from os import path, getenv, makedirs
+from argparse import ArgumentParser
 
 class InvalidModulesVersionException(Exception):
     pass
@@ -11,10 +11,24 @@ class InvalidModulesSyntaxException(Exception):
     pass
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
-    y = open(sys.argv[1], "r").read()
+    parser = ArgumentParser("The C code generator for modular protocol")
+    parser.add_argument('module', help="the yaml file with module description")
+    parser.add_argument('--out', '-o',
+                        help="output path, relative to builddir, defaults to " +
+                             "the module file relative path",
+                        default=None)
+    parser.add_argument('--builddir', '-b',
+                        help="base output directory, defaults to env " +
+                             "BUILDDIR, or current path",
+                        default=getenv('BUILDDIR', '.'))
+    args = parser.parse_args()
+    y = open(args.module, "r").read()
     modules = load(y, Loader)
-    bd = path.dirname(sys.argv[1])
+    bd = args.builddir + '/'
+    if args.out:
+        bd += args.out
+    else:
+        bd += path.dirname(args.module)
     if 'version' not in modules or str(modules['version']) != VERSION:
         raise InvalidModulesVersionException()
 
@@ -22,7 +36,8 @@ if __name__ == "__main__":
         raise InvalidModulesSyntaxException()
 
     for n, m in modules['modules'].items():
-        module = Module(n, m, filename)
+        makedirs(bd, exist_ok=True)
+        module = Module(n, m, args.module)
         print ("Generating " + module.h_name())
         h = module.put_h()
         hf = open(bd + '/' + module.h_name(), "w")
